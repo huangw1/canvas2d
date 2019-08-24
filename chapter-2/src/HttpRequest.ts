66c99ebd69859b94a70727a999b00b93e2faf0a7
huangwenlong <huangwenlong@bigo.sg>
2019-08-24 12:27:46 +0800

    add README

--- chapter-2/src/HttpRequest.ts
+++ chapter-2/src/HttpRequest.ts
@@ -10,7 +10,19 @@ export interface HttpResponse {
 }
 
 export class HttpRequest {
-    public static doGet(url: string, callback: (response: HttpResponse) => void) {
-
+    public static doGet(url: string, callback: (response: HttpResponse) => void, responseType: XMLHttpRequestResponseType = 'text') {
+        const xhr: XMLHttpRequest = new XMLHttpRequest()
+        xhr.responseType = responseType;
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const response: HttpResponse = {success: true, responseType: responseType, response: xhr.response};
+                callback(response);
+            } else {
+                const response: HttpResponse = {success: false, responseType: responseType, response: null};
+                callback(response);
+            }
+        };
+        xhr.open('get', url, true);
+        xhr.send();
     }
 }
