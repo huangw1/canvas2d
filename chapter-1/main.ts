176dbf6c492b43340c0e2af2b36f2df6aedc1d3e
huangwenlong <huangwenlong@bigo.sg>
2019-08-24 16:55:01 +0800

    add parcel

--- chapter-1/main.ts
+++ chapter-1/main.ts
@@ -2,4 +2,4 @@ import {Canvas2DUtil} from "./canvas2D";
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement
 const canvas2D: Canvas2DUtil = new Canvas2DUtil(canvas)
-canvas2D.drawText('Hello World')
+canvas2D.drawText('Hello ff World')
